9036210f9478c874dd5ca323adc107becf45916e
tony <wangxudong@ymatou.com>
2017-04-01 19:37:35 +0800

    add bean utils copy property

--- liveinfo-domain/src/main/java/com/ymatou/liveinfo/domain/service/LiveService.java
+++ liveinfo-domain/src/main/java/com/ymatou/liveinfo/domain/service/LiveService.java
@@ -1,10 +1,15 @@
 package com.ymatou.liveinfo.domain.service;
 
+import com.ymatou.liveinfo.domain.model.ActivityInfo;
 import com.ymatou.liveinfo.domain.model.Live;
 import com.ymatou.liveinfo.domain.repository.LiveRepository;
+import com.ymatou.liveinfo.facade.common.BizException;
+import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.stereotype.Component;
 
+
 import javax.annotation.Resource;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * Created by wangxudong on 2017/4/1.
@@ -20,7 +25,17 @@ public class LiveService {
      * @param sellerId
      * @return
      */
-    public Live getSellerCurrentLive(int sellerId){
-        return liveRepository.getSellerCurrentLive(sellerId);
+    public ActivityInfo getSellerCurrentActivity(int sellerId){
+        Live live = liveRepository.getSellerCurrentLive(sellerId);
+        if(live == null){
+            return  null;
+        }
+        ActivityInfo activityInfo = new ActivityInfo();
+        try {
+            BeanUtils.copyProperties(activityInfo, live);
+            return activityInfo;
+        } catch (Exception e) {
+            throw new BizException("BeanUtils copyProperties Fail,with liveId:" + live.getActivityId());
+        }
     }
 }
